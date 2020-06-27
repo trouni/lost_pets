06ab01ae29b23a2cd79c2a7f581877b083652cb2
Trouni Tiet <trouni@gmail.com>
2020-06-27 17:34:57 +0900

    completed livecode but missing links

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/pets', to: 'pets#index'
+  get '/pets/new', to: 'pets#new', as: :new_pet
+  get '/pets/:id', to: 'pets#show', as: :pet
+  post '/pets', to: 'pets#create'
+  get '/pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  patch '/pets/:id', to: 'pets#update'
+  # delete '/pets/:id', to: 'pets#destroy'
 end
